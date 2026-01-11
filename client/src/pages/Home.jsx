e42df13453418cdfcaf870b339a00daf22b4316c
Pranjal-k22 <pranjalkaran2004@gmail.com>
2026-01-11 14:38:52 +0530

    Testimonial section is completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import FeaturedDestination from '../components/FeaturedDestination'
 import ExclusiveOffers from '../components/ExclusiveOffers'
+import Testimonial from '../components/Testimonial'
 
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
       <Hero />
       <FeaturedDestination/>
       <ExclusiveOffers/>
+      <Testimonial/>
     </>
   )
 }
